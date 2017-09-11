2f5d4121a8f6bc909d8cbe38163e966336242d81
FxFn1131 <FxFn1131@DARKSIDE>
2017-09-11 22:36:57 +0300

    Add project files.

---
+++ Use of Indexers/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * An indexer allows an object to be indexed such as an array. When you define an indexer for a class,
+ *  this class behaves similar to a virtual array.
+ *  You can then access the instance of this class using the array access operator ([ ]).*/
+
+namespace IndexerApplication
+{
+    class IndexedNames
+    {
+        private string[] namelist = new string[size];
+        static public int size = 10;
+        public IndexedNames()
+        {
+            for (int i = 0; i < size; i++)
+                namelist[i] = "N. A.";
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                string tmp;
+
+                if (index >= 0 && index <= size - 1)
+                {
+                    tmp = namelist[index];
+                }
+                else
+                {
+                    tmp = "";
+                }
+
+                return (tmp);
+            }
+            set
+            {
+                if (index >= 0 && index <= size - 1)
+                {
+                    namelist[index] = value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            IndexedNames names = new IndexedNames();
+            names[0] = "Zara";
+            names[1] = "Riz";
+            names[2] = "Nuha";
+            names[3] = "Asif";
+            names[4] = "Davinder";
+            names[5] = "Sunil";
+            names[6] = "Rubic";
+            for (int i = 0; i < IndexedNames.size; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ indexer/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* genel yapı....
+ * element-type this[int index]
+{
+   // The get accessor.
+   get
+   {
+      // return the value specified by index
+   }
+   
+   // The set accessor.
+   set
+   {
+      // set the value specified by index
+   }
+}
+*/
+
+namespace indexer
+{
+    class Insan
+    {
+        static public int size = 10;
+        string[] isimler = new string[size];
+
+        public Insan()
+        {
+            for (int i = 0; i < size; i++)
+                isimler[i] = "N.A";
+        }
+       
+
+        public string this[int i]
+        {
+            get
+            {
+                return isimler[i];
+            }
+
+            set
+            {
+
+                isimler[i] = value;
+            }
+        }
+
+
+
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Insan insan = new Insan();
+            insan[3] = "Erolca";
+            insan[5] = "Erol";
+
+            Console.WriteLine(insan[3]);
+            Console.WriteLine(insan[5]);
+
+
+
+
+        }
+    }
+}
